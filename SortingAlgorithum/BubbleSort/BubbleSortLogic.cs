e68d3c2eee053dfe0bd45ad4e8442334bedc045b
unknown <bhotnikhil25@gmail.com>
2023-03-27 14:59:14 +0530

    [Nikhil]-Bubble Sort

---
+++ SortingAlgorithum/BubbleSort/BubbleSortLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    public class BubbleSortLogic
+    {
+        public static void bubbleSortLogic(int[] arr) 
+        { 
+            int n=arr.Length;
+            for(int i=0;i<n-1; i++) 
+            {
+               int flag = 0;
+                for(int j=0;j<n-1-i;j++) //j<n-1-i used for last number is sorted so we decrement that
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp; 
+                        flag = 1;  //if swapping is done then we set to 1
+                    }
+                }
+                if (flag == 0)  //
+                    break;
+            }
+        }
+    }
+}
---
+++ SortingAlgorithum/BubbleSort/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace BubbleSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wel-Come To Bubble Sort");
+        
+            Console.WriteLine("How Many Number You Want To Sort Using Buuble Sort");
+            int num=Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[num];
+           
+            for(int i=0; i<num; i++)
+            {
+                Console.WriteLine("Enter the {0} Number",i+1);
+                arr[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Array Before Sorting");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            BubbleSortLogic.bubbleSortLogic(arr);
+            Console.WriteLine("Array After Sorting");
+            for (int i=0;i<arr.Length;i++)
+            {
+                Console.WriteLine(arr[i]+" ");
+            }
+        }
+    }
+}
