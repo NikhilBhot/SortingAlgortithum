8ecd417c33d250f8c13ae95c505d5336933d507e
unknown <bhotnikhil25@gmail.com>
2023-03-27 15:49:44 +0530

    [Nikhil]-Insertion Sort

---
+++ SortingAlgorithum/InsertionSort/InsertionSortLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    public class InsertionSortLogic
+    {
+        public static void insertionSortLogic(int[] arr) 
+        {
+            int n=arr.Length;
+            for(int i=1;i<n;i++) //this for start with 1 beacause 0 th elemement we consider as soerted 
+            {
+                int temp = arr[i]; //store the value which we have to sort
+                int j = i - 1;
+                while(j >= 0 && arr[j]>temp) //this while loop going on upto we didint get correct postion 
+                {
+                    arr[j + 1] = arr[j];
+                    j--;  //we shift the element for correct postioon
+                }
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
---
+++ SortingAlgorithum/InsertionSort/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace InsertionSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wel-Come To Insertion Sort");
+
+            Console.WriteLine("How Many Number You Want To Sort Using Buuble Sort");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Enter the {0} Number", i + 1);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Array Before Sorting");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            InsertionSortLogic.insertionSortLogic(arr);
+            Console.WriteLine("Array After Sorting");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+        }
+    }
+}
